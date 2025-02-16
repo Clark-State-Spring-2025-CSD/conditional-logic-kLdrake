429c394d65b2d568c7e7621f0fe115f1fe7d7c35
Kris Morrow <194855958+kLdrake@users.noreply.github.com>
2025-02-15 19:26:13 -0500

    Update video.py

--- video.py
+++ video.py
@@ -1,3 +1,29 @@
+
+print("Welcome to the Menu Ordering System")
+
+customerAge = int(input("What is the age of the customer?"))
+
+price = 0
+
+if customerAge < 12:
+    price = 8
+elif customerAge >= 62:
+    price = 9
+else:
+    price = 12
+
+print(f"The cost for this customer is ${price}.")
+
+drinkYesNo = input("Add a drink? Y/N? ")
+
+if drinkYesNo == "Y":
+    smallLarge = input("Small or Large? S/L? ")
+    if smallLarge == "L":
+        price += 2
+    else:
+        price += 1
+
+print (f"Total is: ${price}")
 #Updated 1 Feb 2025
 #Watch the video here: https://www.youtube.com/watch?v=YKxvDL3JxEA or follow the instructor led demo
 #Implement all the code shown in the video.
